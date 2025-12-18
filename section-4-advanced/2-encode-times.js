d7c8a1ffad46b75161bef30b9916edd8941f1a4d
yerayafonso <yafonso02@gmail.com>
2025-12-18 16:58:02 +0000

    complete advanced challenge 2

--- section-4-advanced/2-encode-times.js
+++ section-4-advanced/2-encode-times.js
@@ -21,7 +21,36 @@ So we could represent the data above by a string in the following way:
 Implement a function that will take an array of time intervals for being awake/asleep over 60 mins and will generate a string encoding this information with a series of . and #
 */
 
-function encodeTimes(times) {}
+function encodeTimes(times) {
+  times.push(60);
+  console.log(times);
+  let timeDiff = [];
+
+  for (let i = 1; i < times.length; i++) {
+    let diff = times[i] - times[i - 1];
+    timeDiff.push(diff);
+  }
+  console.log(timeDiff);
+  let data = "";
+
+  for (let j = 0; j < timeDiff.length; j++) {
+    if (j % 2 === 0) {
+      for (let i = 0; i < timeDiff[j]; i++) {
+        data += ".";
+      }
+    } else {
+      for (let i = 0; i < timeDiff[j]; i++) {
+        data += "#";
+      }
+    }
+  }
+
+  console.log(data);
+  return data;
+}
+
+encodeTimes([0, 24, 29]);
+
 // Once you've passed the tests we've written, add some more test to this challenge too!
 
 module.exports = encodeTimes;
